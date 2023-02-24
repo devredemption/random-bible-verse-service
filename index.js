66009c1477dbc32e88d65bf569630a73b084b35d
devredemption <redemptionjonathan@yahoo.com>
2023-02-24 01:57:09 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const axios_1 = __importDefault(require("axios"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const PORT = process.env.PORT;
+const app = (0, express_1.default)();
+app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const verson_response = yield (yield axios_1.default.get("https://bible-search.p.rapidapi.com/random-verse", {
+            headers: {
+                "X-RapidAPI-Key": process.env.API_KEY,
+                "X-RapidAPI-Host": "bible-search.p.rapidapi.com",
+            },
+        })).data[0];
+        const verse = {
+            text: verson_response.text,
+            display_ref: `${verson_response.book_name} ${verson_response.chapter}:${verson_response.verse}`,
+        };
+        res.json(verse);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}));
+app.listen(PORT, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+});
---
+++ index.ts
@@ -0,0 +1,33 @@
+import express, { Express, Request, Response } from "express";
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const app: Express = express();
+
+app.get("/", async (req: Request, res: Response) => {
+	try {
+		const verson_response = await (
+			await axios.get("https://bible-search.p.rapidapi.com/random-verse", {
+				headers: {
+					"X-RapidAPI-Key": process.env.API_KEY,
+					"X-RapidAPI-Host": "bible-search.p.rapidapi.com",
+				},
+			})
+		).data[0];
+		const verse = {
+			text: verson_response.text,
+			display_ref: `${verson_response.book_name} ${verson_response.chapter}:${verson_response.verse}`,
+		};
+		res.json(verse);
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+app.listen(PORT, () => {
+	console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+});
